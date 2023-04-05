59d5ec4dd68f481033ff415375d1cd6f5c5e6dcd
getssh <gtamrat33@gmail.com>
2023-04-05 10:17:24 +0300

    Add modal functionality for confirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
 import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+
 function App() {
+  const {cartItems} = useSelector((store) => store.cart);
+  const {isOpen} = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems])
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
